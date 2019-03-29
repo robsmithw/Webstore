a3e31c4d597db28aa4122ce78213ba8a8e041a50
a <a>
2019-03-29 16:05:41 -0500

    Initialized react app ready to be implemented.

---
+++ frontend/src/components/AllNews.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class AllNews extends Component {
+  render() {
+    return this.props.map((item) => (
+        <h1>Hello World</h1>
+    ));
+  }
+}
+
+export default AllNews;
