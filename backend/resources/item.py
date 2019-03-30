8ef84edc4e564893aea0c0f2870fe5d1750f713e
a <a>
2019-03-29 21:43:38 -0500

    Weird bug with relative path names so data directly put in.

--- backend/resources/item.py
+++ backend/resources/item.py
@@ -1,8 +1,95 @@
 from flask_restful import Resource
 
 class Item(Resource):
-    def get(self):
-        return
+    def get(self): #will query from db in future
+        return {
+    "for_sell":[
+    {
+    "id" : 1,       
+    "item_name": "Basketball",
+    "price": 30,
+    "quantity":1,
+    "status":"new",
+    "instock": True,
+    "Type" : [{
+        "Sell": True,
+        "rent": False
+    }]
+    }, 
+    {
+    "id" : 2,       
+    "item_name": "HDMI Cable",
+    "price": 5,
+    "quantity":0,
+    "status":"used",
+    "instock": False,
+    "Type" : [{
+        "Sell": True,
+        "rent": False
+    }]
+    }, 
+    {
+    "id" : 3,       
+    "item_name": "VGA Cable",
+    "price": 3,
+    "quantity":0,
+    "status":"new",
+    "instock": False,
+    "Type" : [{
+        "Sell": True,
+        "rent": False
+    }]
+    }, 
+    {
+    "id" : 4,       
+    "item_name": "24 inch monitor",
+    "price": 200,
+    "quantity":1,
+    "status":"new",
+    "instock": True,
+    "Type" : [{
+        "Sell": True,
+        "rent": False
+    }]
+    }, 
+    {
+    "id" : 5,       
+    "item_name": "Speaker",
+    "price": 50,
+    "quantity":1,
+    "status":"used",
+    "instock": True,
+    "Type" : [{
+        "Sell": True,
+        "rent": False
+    }]
+    }, 
+    {
+    "id" : 6,       
+    "item_name": "Lightning Cable",
+    "price": 10,
+    "quantity":1,
+    "status":"new",
+    "instock": True,
+    "Type" : [{
+        "Sell": True,
+        "rent": False
+    }]
+    }, 
+    {
+    "id" : 7,       
+    "item_name": "A game",
+    "price": 60,
+    "quantity":1,
+    "status":"new",
+    "instock": True,
+    "Type" : [{
+        "Sell": True,
+        "rent": True
+    }]
+    }
+    ]
+}
     def post(self):
         return
     def put(self):
--- backend/resources/user.py
+++ backend/resources/user.py
@@ -1,8 +1,45 @@
 from flask_restful import Resource
+import os
+import json
 
 class User(Resource):
-    def get(self):
-        return
+    def get(self): #In the future I will be setting this up to connect to a db
+        return {
+    "Users":[
+        {
+            "id":1,
+            "user_name": "a_user_name",
+            "password": "a_hashed_value",
+            "selling":[{
+                "item_id":1
+            },
+            {"item_id" : 2
+            }]
+        },
+        {
+            "id":3,
+            "user_name": "a_user_name",
+            "password": "a_hashed_value",
+            "selling":[{
+                "item_id":3
+            },
+            {"item_id" : 4
+            }]
+        },
+        {
+            "id":3,
+            "user_name": "a_user_name",
+            "password": "a_hashed_value",
+            "selling":[{
+                "item_id":5
+            },
+            {"item_id" : 6
+            },{
+                "item_id":7
+            }]
+        }
+    ]
+}
     def post(self):
         return
     def put(self):
