cbc2cff983a731bd6f4ad1d331b121d20878e9c5
a <a>
2019-03-29 19:59:56 -0500

    Added json files for mock data and initialized API

---
+++ backend/resources/item.py
@@ -0,0 +1,11 @@
+from flask_restful import Resource
+
+class Item(Resource):
+    def get(self):
+        return
+    def post(self):
+        return
+    def put(self):
+        return
+    def delete(self):
+        return
