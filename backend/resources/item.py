d0284e16f68e3f52ef129f87a08a2c85be2b23ac
Robert Smith <dontspammer@gmail.com>
2019-05-28 14:30:19 -0500

    Changed password to come from a file and finished gets for User/Item.

--- backend/resources/item.py
+++ backend/resources/item.py
@@ -1,10 +1,17 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from mock_data.query import query_db
 
 connected = False
 class Item(Resource):
     def get(self):
-        return query_db("SELECT * FROM Item",connected)
+        parser = reqparse.RequestParser()
+        parser.add_argument('item', type=str) #Returns a specific user by specified by id
+        args = parser.parse_args()
+        if args["item"]:
+            select_statement = "SELECT * FROM Item WHERE `id` = {}".format(args["item"])
+            return query_db(select_statement,connected)
+        select_statement = "SELECT * FROM Item"
+        return query_db(select_statement,connected)
     def post(self):
         return
     def put(self, item_id):
