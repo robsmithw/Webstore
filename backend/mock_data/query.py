cc41c3c27fd8c05635a0a5e94d844c3fe60e884f
Robert Smith <dontspammer@gmail.com>
2019-05-28 14:24:12 -0500

    Changed password to come from a file and finished gets for User/Item.

--- backend/mock_data/query.py
+++ backend/mock_data/query.py
@@ -16,12 +16,13 @@ def test_connect(the_password,connection_status):
 
 def query_db(sql_statement,is_connected):
     try:
-        if(not is_connected):
-            password = (str(sys.argv[1])) #We have user provide password for super_user through command line
+        if not is_connected:
+            with open("password.txt",'r') as pass_file:
+                password = pass_file.readline()
             our_connection = test_connect(password,is_connected)
 
-    except IndexError:
-        print("Please ensure you're providing the password for super_user through command line.")
+    except FileNotFoundError: #Need to eventually make sure this catch still works
+        print("Please make sure you have a file to store the password")
         exit(0)
 
     try:
@@ -29,8 +30,10 @@ def query_db(sql_statement,is_connected):
         with our_connection.cursor() as cursor:
             cursor.execute(sql_statement)
             if("SELECT" in sql_statement):
-                results = cursor.fetchone()
-                print(results)
+                results = []
+                for result in cursor.fetchall():
+                    results.append(result)
+                return results
         our_connection.commit()
     except Exception as e:
         success = False
--- backend/resources/item.py
+++ backend/resources/item.py
@@ -1,98 +1,13 @@
 from flask_restful import Resource
+from mock_data.query import query_db
 
+connected = False
 class Item(Resource):
-    def get(self): #will query from db in future
-        return {
-    "for_sell":[
-    {
-    "id" : 1,       
-    "item_name": "Basketball",
-    "price": 30,
-    "quantity":1,
-    "status":"new",
-    "instock": True,
-    "Type" : [{
-        "Sell": True,
-        "rent": False
-    }]
-    }, 
-    {
-    "id" : 2,       
-    "item_name": "HDMI Cable",
-    "price": 5,
-    "quantity":0,
-    "status":"used",
-    "instock": False,
-    "Type" : [{
-        "Sell": True,
-        "rent": False
-    }]
-    }, 
-    {
-    "id" : 3,       
-    "item_name": "VGA Cable",
-    "price": 3,
-    "quantity":0,
-    "status":"new",
-    "instock": False,
-    "Type" : [{
-        "Sell": True,
-        "rent": False
-    }]
-    }, 
-    {
-    "id" : 4,       
-    "item_name": "24 inch monitor",
-    "price": 200,
-    "quantity":1,
-    "status":"new",
-    "instock": True,
-    "Type" : [{
-        "Sell": True,
-        "rent": False
-    }]
-    }, 
-    {
-    "id" : 5,       
-    "item_name": "Speaker",
-    "price": 50,
-    "quantity":1,
-    "status":"used",
-    "instock": True,
-    "Type" : [{
-        "Sell": True,
-        "rent": False
-    }]
-    }, 
-    {
-    "id" : 6,       
-    "item_name": "Lightning Cable",
-    "price": 10,
-    "quantity":1,
-    "status":"new",
-    "instock": True,
-    "Type" : [{
-        "Sell": True,
-        "rent": False
-    }]
-    }, 
-    {
-    "id" : 7,       
-    "item_name": "A game",
-    "price": 60,
-    "quantity":1,
-    "status":"new",
-    "instock": True,
-    "Type" : [{
-        "Sell": True,
-        "rent": True
-    }]
-    }
-    ]
-}
+    def get(self):
+        return query_db("SELECT * FROM Item",connected)
     def post(self):
         return
-    def put(self):
+    def put(self, item_id):
         return
-    def delete(self):
+    def delete(self, item_id):
         return
