49e76157eba154cb105b174511166d684d89d07e
Robert Smith <dontspammer@gmail.com>
2019-05-28 21:58:17 -0500

    Initial dirty post functions, need to clean up and look into hashing.

--- backend/resources/user.py
+++ backend/resources/user.py
@@ -1,5 +1,6 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource, reqparse, request
 from mock_data.query import query_db
+from hashlib import pbkdf2_hmac
 
 connected = False
 class User(Resource):
@@ -11,9 +12,13 @@ class User(Resource):
             select_statement = "SELECT * FROM User WHERE `id` = {}".format(args["user"])
             return query_db(select_statement,connected)
         select_statement = "SELECT * FROM User"
-        return query_db(select_statement,connected)
-    def post(self):
-        return
+        return query_db(select_statement,connected), 200
+    def post(self): #Need to look into hashing passwords using pbkdf2_hmac
+        insert_users = "INSERT INTO `User` (`user_name`,`password`) VALUES ('{}', '{}')"
+        name = request.json["user_name"]
+        user_pass = request.json["password"]
+        insert_statement = insert_users.format(name,user_pass)
+        return query_db(insert_statement, connected), 201
     def put(self, user_id):
         return
     def delete(self, user_id):
